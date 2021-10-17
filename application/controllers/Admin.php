34ee806b23363730925eac93c1504df63f6444e5
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-10-17 12:29:56 +0700

    crud suratmasuk

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -35,6 +35,11 @@ class Admin extends CI_Controller{
         $this->load->view('suratmasuk/input_suratmasuk');
     }
 
+    public function edit_suratmasuk()
+    {
+        $this->load->view('suratmasuk/edit_suratmasuk');
+    }
+
     public function add_suratmasuk()
     {
         $query = $this->suratmasuk_model->save();
@@ -48,4 +53,28 @@ class Admin extends CI_Controller{
 
         }
 	}
+
+    public function update_suratmasuk()
+    {
+        $query = $this->suratmasuk_model->update();
+        if ($query == true) {
+            $this->session->set_flashdata("success","Data berhasil di Edit");
+            redirect('admin/suratmasuk');       
+        }else{
+            $db_error =$this->db->error();
+            $this->session->set_flashdata("failed",$db_error['message']);
+            redirect('admin/suratmasuk');       
+
+        }
+    }
+
+    public function delete_suratmasuk()
+    {
+        $this->suratmasuk_model->delete();
+    }
+
+    public function detail_suratmasuk()
+    {
+        $this->load->view('suratmasuk/detail_suratmasuk');
+    }
 }
