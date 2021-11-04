801ec8fda0036d43a0a384d54e71409f5dbcd8e8
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-11-04 21:00:09 +0700

    revisi db

--- db_surat.sql
+++ db_surat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2021 at 10:00 AM
+-- Generation Time: Nov 04, 2021 at 02:59 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.22
 
@@ -31,17 +31,16 @@ CREATE TABLE `tb_admin` (
   `id_admin` int(10) NOT NULL,
   `nama_admin` varchar(50) NOT NULL,
   `username_admin` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `gambar` varchar(255) NOT NULL
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_admin`
 --
 
-INSERT INTO `tb_admin` (`id_admin`, `nama_admin`, `username_admin`, `password`, `gambar`) VALUES
-(1, 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin.jpg'),
-(2, 'admin2', 'admin2', '315f166c5aca63a157f7d41007675cb44a948b33', 'admin2.jpg');
+INSERT INTO `tb_admin` (`id_admin`, `nama_admin`, `username_admin`, `password`) VALUES
+(1, 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997'),
+(2, 'admin2', 'admin2', '315f166c5aca63a157f7d41007675cb44a948b33');
 
 -- --------------------------------------------------------
 
@@ -100,35 +99,34 @@ CREATE TABLE `tb_bagian` (
   `nama_lengkap` varchar(70) NOT NULL,
   `tanggal_lahir_bagian` date NOT NULL,
   `alamat` text NOT NULL,
-  `no_hp_bagian` varchar(12) NOT NULL,
-  `gambar` varchar(255) NOT NULL
+  `no_hp_bagian` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_bagian`
 --
 
-INSERT INTO `tb_bagian` (`id_bagian`, `nama_bagian`, `username_admin_bagian`, `password_bagian`, `nama_lengkap`, `tanggal_lahir_bagian`, `alamat`, `no_hp_bagian`, `gambar`) VALUES
-(1, 'WALIKOTA', 'walikota', 'daaf9486f7039e0087f268db4215998bfb6185cb', 'walikota', '2017-05-25', 'samarinda', '081222222222', 'walikota.jpg'),
-(2, 'WAKIL WALIKOTA', 'wawali', '61e1322f11261e3c4c9c8631f86bc70237c5ee42', 'wawali', '2017-11-04', 'samarinda', '081222222222', 'wawali.jpg'),
-(3, 'SEKDA', 'sekda', 'ba09b363755a11c5a46f1be9a8dd6e579a1c75b7', 'SEKDA', '2017-11-10', 'Samarinda', '081299101309', 'sekda.jpg'),
-(4, 'PLH.SEKDA', 'plh sekda', 'ce26252389e1562bdd0a4094270b2cfb2d16f6be', 'plh sekda', '2017-11-10', 'Samarinda', '087731311111', 'plh sekda.jpg'),
-(5, 'ASS.I', 'ass i', '972a3c20bb3d97797348df57ec7d81185de5ce17', 'ass i', '2017-11-10', 'samarinda', '322222222222', 'ass i.jpg'),
-(6, 'PLT.ASS.I', 'plt ass i', '4182b75e1a82d3e2d1751e9d903eefad6a80d212', 'plt ass i', '2017-11-03', 'samarinda', '233333333333', 'plt ass i.jpg'),
-(7, 'PLT.ASS.II', 'plt ass ii', '3f17afaf0a4238fdd1ddc0c3a88686fb96f5c68c', 'plt ass ii', '2017-11-10', 'bpp', '344444444444', 'plt ass ii.jpg'),
-(8, 'ASS.III', 'ass iii', 'fed0054dcfee81465402afa25f65e9f66cb697af', 'ass iii', '2017-11-01', 'bpp', '222222222222', 'ass iii.png'),
-(9, 'UMUM', 'umum', 'b617726c7f45ecb196ef74881089fa17d90d7276', 'umum', '2017-11-17', 'smd', '333333333333', 'umum.jpg'),
-(10, 'TU', 'tu', 'a3da4c6307d230e1f1c8ad62e00d05ff1f1f5b52', 'tu', '2017-11-10', 'badak', '24224242', 'tu.png'),
-(11, 'KESRA', 'kesra', '983a6611e9d0f6e5601896247285eb2858ff46bd', 'kesra', '2017-11-17', 'bpp', '098979989999', 'kesra.png'),
-(12, 'ADM.PEMB', 'adm pemb', '263bca10cf6fe2407537f3da354c2a5bd41f0046', 'adm pemb', '2017-11-07', 'samarinda', '089836323711', 'adm pemb.jpg'),
-(13, 'ORTAL', 'ortal', '1e760e356d62c56a70893e1d3843c1c89b4a2212', 'ortal', '2017-11-13', 'samarinda', '081299101309', 'ortal.jpg'),
-(14, 'PEM-OTDA', 'pem-otda', '278f69af54d64f658a430ecc6e060a9c936cba75', 'pem-otda', '2017-11-13', 'samarinda', '081222222222', 'pem-otda.jpg'),
-(15, 'EKONOMI & SDA', 'ekonomi', 'edbc608bc611081ad29a586f1a328fc553a83cb5', 'ekonomi', '2017-11-15', 'samarinda', '081299101309', 'ekonomi.jpg'),
-(16, 'HUKUM', 'hukum', 'ab86e34c8b761c9e534f9c020f83cb927c1ad673', 'hukum', '2017-11-15', 'Balikpapan', '081222222222', 'hukum.png'),
-(17, 'BKPPD', 'bkppd', '50818d7e0331a1dec698c35c32af22556f90fb1a', 'bkppd', '2017-12-01', 'samarinda', '081299101309', 'bkppd.png'),
-(18, 'PROTOKOL', 'protokol', 'a50492e61cab897b02e491d340de4083cb74f537', 'protokol', '2017-12-01', 'samarinda', '081242424223', 'protokol.png'),
-(19, 'HUMAS', 'humas', 'b4c4048e1c66effb3ac466b6f33df58c0e93672c', 'humas', '2017-12-01', 'samarinda', '081344444444', 'humas.png'),
-(20, 'INFRASTRUKTUR', 'infrastruktur', 'cc48a8c41985da07efc915fea8fc0ac3bec7f0e9', 'infrastruktur', '2017-12-01', 'smd', '083455253225', 'infrastruktur.jpg');
+INSERT INTO `tb_bagian` (`id_bagian`, `nama_bagian`, `username_admin_bagian`, `password_bagian`, `nama_lengkap`, `tanggal_lahir_bagian`, `alamat`, `no_hp_bagian`) VALUES
+(1, 'WALIKOTA', 'walikota', 'daaf9486f7039e0087f268db4215998bfb6185cb', 'walikota', '2017-05-25', 'samarinda', '081222222222'),
+(2, 'WAKIL WALIKOTA', 'wawali', '61e1322f11261e3c4c9c8631f86bc70237c5ee42', 'wawali', '2017-11-04', 'samarinda', '081222222222'),
+(3, 'SEKDA', 'sekda', 'ba09b363755a11c5a46f1be9a8dd6e579a1c75b7', 'SEKDA', '2017-11-10', 'Samarinda', '081299101309'),
+(4, 'PLH.SEKDA', 'plh sekda', 'ce26252389e1562bdd0a4094270b2cfb2d16f6be', 'plh sekda', '2017-11-10', 'Samarinda', '087731311111'),
+(5, 'ASS.I', 'ass i', '972a3c20bb3d97797348df57ec7d81185de5ce17', 'ass i', '2017-11-10', 'samarinda', '322222222222'),
+(6, 'PLT.ASS.I', 'plt ass i', '4182b75e1a82d3e2d1751e9d903eefad6a80d212', 'plt ass i', '2017-11-03', 'samarinda', '233333333333'),
+(7, 'PLT.ASS.II', 'plt ass ii', '3f17afaf0a4238fdd1ddc0c3a88686fb96f5c68c', 'plt ass ii', '2017-11-10', 'bpp', '344444444444'),
+(8, 'ASS.III', 'ass iii', 'fed0054dcfee81465402afa25f65e9f66cb697af', 'ass iii', '2017-11-01', 'bpp', '222222222222'),
+(9, 'UMUM', 'umum', 'b617726c7f45ecb196ef74881089fa17d90d7276', 'umum', '2017-11-17', 'smd', '333333333333'),
+(10, 'TU', 'tu', 'a3da4c6307d230e1f1c8ad62e00d05ff1f1f5b52', 'tu', '2017-11-10', 'badak', '24224242'),
+(11, 'KESRA', 'kesra', '983a6611e9d0f6e5601896247285eb2858ff46bd', 'kesra', '2017-11-17', 'bpp', '098979989999'),
+(12, 'ADM.PEMB', 'adm pemb', '263bca10cf6fe2407537f3da354c2a5bd41f0046', 'adm pemb', '2017-11-07', 'samarinda', '089836323711'),
+(13, 'ORTAL', 'ortal', '1e760e356d62c56a70893e1d3843c1c89b4a2212', 'ortal', '2017-11-13', 'samarinda', '081299101309'),
+(14, 'PEM-OTDA', 'pem-otda', '278f69af54d64f658a430ecc6e060a9c936cba75', 'pem-otda', '2017-11-13', 'samarinda', '081222222222'),
+(15, 'EKONOMI & SDA', 'ekonomi', 'edbc608bc611081ad29a586f1a328fc553a83cb5', 'ekonomi', '2017-11-15', 'samarinda', '081299101309'),
+(16, 'HUKUM', 'hukum', 'ab86e34c8b761c9e534f9c020f83cb927c1ad673', 'hukum', '2017-11-15', 'Balikpapan', '081222222222'),
+(17, 'BKPPD', 'bkppd', '50818d7e0331a1dec698c35c32af22556f90fb1a', 'bkppd', '2017-12-01', 'samarinda', '081299101309'),
+(18, 'PROTOKOL', 'protokol', 'a50492e61cab897b02e491d340de4083cb74f537', 'protokol', '2017-12-01', 'samarinda', '081242424223'),
+(19, 'HUMAS', 'humas', 'b4c4048e1c66effb3ac466b6f33df58c0e93672c', 'humas', '2017-12-01', 'samarinda', '081344444444'),
+(20, 'INFRASTRUKTUR', 'infrastruktur', 'cc48a8c41985da07efc915fea8fc0ac3bec7f0e9', 'infrastruktur', '2017-12-01', 'smd', '083455253225');
 
 -- --------------------------------------------------------
 
@@ -238,6 +236,25 @@ INSERT INTO `tb_suratmasuk` (`id_suratmasuk`, `tanggalmasuk_suratmasuk`, `kode_s
 (37, '2021-10-17 21:09:00', '1', '4525', '1', '2021-10-17', '1', '1', '1', '2021-4525.pdf', 'admin', '2021-10-17 21:10:12', '', '1970-01-01 07:00:00', '', '1970-01-01 07:00:00', '', '1970-01-01 07:00:00'),
 (38, '2021-10-17 21:11:00', '1', '4526', '1', '2021-10-17', '1', '1', '1', '2021-4526.pdf', 'admin', '2021-10-17 21:11:26', '', '1970-01-01 07:00:00', '', '1970-01-01 07:00:00', '', '1970-01-01 07:00:00');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_tu`
+--
+
+CREATE TABLE `tb_tu` (
+  `id_tu` int(11) NOT NULL,
+  `nama_tu` varchar(60) NOT NULL,
+  `ttd` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_tu`
+--
+
+INSERT INTO `tb_tu` (`id_tu`, `nama_tu`, `ttd`) VALUES
+(2, 'Isnan Akbar, A.Md.,', 'ttd.png');
+
 --
 -- Indexes for dumped tables
 --
@@ -291,6 +308,12 @@ ALTER TABLE `tb_suratmasuk`
   ADD UNIQUE KEY `nomorurut_suratmasuk` (`nomorurut_suratmasuk`);
 
 --
+-- Indexes for table `tb_tu`
+--
+ALTER TABLE `tb_tu`
+  ADD PRIMARY KEY (`id_tu`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -298,7 +321,7 @@ ALTER TABLE `tb_suratmasuk`
 -- AUTO_INCREMENT for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tb_arsip_suratkeluar`
@@ -335,6 +358,12 @@ ALTER TABLE `tb_suratkeluar`
 --
 ALTER TABLE `tb_suratmasuk`
   MODIFY `id_suratmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `tb_tu`
+--
+ALTER TABLE `tb_tu`
+  MODIFY `id_tu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
